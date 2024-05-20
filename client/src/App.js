37c6803926a01e8e460f441b69213f8d5946b83f
JATIN <117251224+CODExJATIN@users.noreply.github.com>
2024-05-20 20:22:23 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React,{useState,useEffect} from 'react';
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>SparkUp</h1>
+      <h3>I hope we will start the work from tomorrow</h3>
+      <p>Let's see how it goes........</p>
+    </div>
+  );
+}
+
+export default App;
